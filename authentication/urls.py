139c9bab02c858010600abaeec7ceb52b9a0c065
Swasthani Shrestha <np03cs4a220235@heraldcollege.edu.np>
2024-03-29 20:48:08 +0545

    Initial Commit

---
+++ authentication/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+from authentication.views import index
+from authentication.views import category_list_view, index, product_list_view, dashboard_consumer_view
+
+app_name = "authentication"
+
+urlpatterns = [
+    path('',views.home, name="home"),
+    path('signup',views.signup, name="signup"),
+    path('signin',views.signin, name="signin"),
+    path('signout',views.signout, name="signout"),
+    path('activate/<uidb64>/<token>',views.activate, name="activate"),
+    path('products/',views.product_list_view, name = "product-list"),
+    path('category/', views.category_list_view, name = "category-list"),
+    path('dashboard/', views.dashboard_consumer_view, name = "consumer-dashboard" ),
+    path('admin-dashboard/', views.dashboard_admin_view, name = "admin-dashboard" )
+]
+if settings.DEBUG:
+
+    urlpatterns += static(settings.STATIC_URL, documents_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, documents_root = settings.MEDIA_ROOT)
